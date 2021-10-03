bddbd2bcb0e03f809f10a2d3acf57e5bb89e3b75
AshGw <AshrefGw@proton.me>
2021-10-03 15:57:10 +0100

    rebase utils

--- utils.h
+++ utils.h
@@ -9,6 +9,7 @@ void tracker(void);
 #include <string.h>
 #include <unistd.h>
 
+// append buffer: my custom mutable strings in C
 #ifndef ABUF_INIT
 #define ABUF_INIT                                                              \
   { NULL, 0 }
@@ -18,33 +19,34 @@ struct abuf {
   int len;
 };
 
-void ab_append(struct abuf *, const char *s, int len);
-void ab_free(struct abuf *ab);
+void abAppend(struct abuf *, const char *s, int len);
+void abFree(struct abuf *ab);
 #endif
 
 // Utilities functions
-void clear_screen(void);
+void clearScreen(void);
 
 #include <errno.h>
 void die(const char *);
 
-#ifndef _POSIX_C_SOURCE
+
+#ifndef _POSIX_C_SOURCE 
 #define _POSIX_C_SOURCE -1
 #endif
 
-#if !_POSIX_C_SOURCE >= 200809L || !defined _GNU_SOURCE
+#if !_POSIX_C_SOURCE >= 200809L || ! defined _GNU_SOURCE 
 // My own implementation of strnlen_s()
-// return the number of the byte pointed to by s, excluding '\0' but
+// return the number of the byte pointed to by s, excluding '\0' but 
 // at most len
 size_t strnlen_s(const char *s, size_t maxlen);
 #endif
 
 // an implementation of getline()
-// getline() is included in
-// Since glibc 2.10:
-//     _POSIX_C_SOURCE >= 200809L
-// Before glibc 2.10:
-//     _GNU_SOURCE
+// getline() is included in 
+           // Since glibc 2.10:
+           //     _POSIX_C_SOURCE >= 200809L
+           // Before glibc 2.10:
+           //     _GNU_SOURCE
 
 #if !defined _POSIX_C_SOURCE || !defined _GNU_SOURCE
 #include <stdio.h>
