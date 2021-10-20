32133699a07298e8ebbc5af68dd9c7db12f025ac
AshGw <AshrefGw@proton.me>
2021-10-20 15:14:08 +0100

    cleanup terminal

--- terminal.h
+++ terminal.h
@@ -1,10 +1,10 @@
-#ifndef TERMINAL_H
-#define TERMINAL_H 1
 #include <errno.h>
 #include <stdlib.h>
 #include <sys/ioctl.h>
 #include <termios.h>
 #include <unistd.h>
+#ifndef TERMINAL_H
+#define TERMINAL_H 1
 
 int getCursorPosition(unsigned int *, unsigned int *);
 void getWindowSize(unsigned int *, unsigned int *);
